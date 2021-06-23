4a86bc8808f8ee536a98bde35c35f152b325422c
Graeme <graemster@gmail.com>
2021-06-23 03:29:01 -0500

    highlight current link

--- starter-code/app/js/script.js
+++ starter-code/app/js/script.js
@@ -6,26 +6,35 @@ const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 
-btnHamburger.addEventListener('click', function(){
+btnHamburger.addEventListener('click', function () {
+	if (header.classList.contains('open')) {
+		// Close Hamburger Menu
+		body.classList.remove('noscroll');
+		header.classList.remove('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+	} else {
+		// Open Hamburger Menu
+		body.classList.add('noscroll');
+		header.classList.add('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
 
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
-    });
-    
-  }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-
-  }  
+// iterate over each link tag and apply .current-link class to the one whose href matches current page's URL
+console.log('url', document.URL);
+const regex = /[^/]+$/;
+const everythingAfterSlash = document.URL.match(regex);
+console.log('url', everythingAfterSlash[0]);
+const currentLinks = document.querySelectorAll('a[href="' + everythingAfterSlash[0] + '"]');
+console.log('currentLinks', currentLinks);
+currentLinks.forEach(function (link) {
+	link.className += ' current-link';
 });
 
 // // Change img src for hamburger menu
